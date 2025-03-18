67b59c8d0c17b19c03f058bffef6684e55dae743
Moussa Badr <121777237+badr21k@users.noreply.github.com>
2025-03-18 12:37:28 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const GAS_URL = "https://script.google.com/macros/s/AKfycbwYHViwwzZOUGKmliDgEur7jWeMFHtCkCcxp24iXhRbNBXp8k8ZyOmsUXC4EUP-b871/exec"; // Replace with your Apps Script URL
+const GAS_URL = "https://script.google.com/macros/s/AKfycbwv3tzXHIuUiy0KFGj8S23JL_MIBbB5XDya0dZo5_I87F2bRnAHu5h2jOqkLH2Kszbc/exec"; // Replace with your Apps Script URL
 
 function getDayIndex(dayName) {
     const dayOfWeekMap = { 'Sunday': 0, 'Monday': 1, 'Tuesday': 2, 'Wednesday': 3, 'Thursday': 4, 'Friday': 5, 'Saturday': 6 };
